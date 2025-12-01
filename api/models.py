a7ac176a7550eb6c228a37ac7e0d7482ae28f3fe
BlueBird <safwan.proffx@gmail.com>
2025-12-01 22:23:44 +0600

    Updated the models.py

--- api/models.py
+++ api/models.py
@@ -1,4 +1,4 @@
-from pydantic import BaseModel, constr, validator
+from pydantic import BaseModel, constr
 
 SEVERITY_CHOICES = {"Low", "Medium", "High", "Critical"}
 STATUS_CHOICES = {"Open", "Fixed", "Accepted", "False Positive"}
